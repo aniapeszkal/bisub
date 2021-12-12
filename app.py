f99348d43a06be10ba67ef24c03b264d7bd639e9
Ania <anna.peszkal@gmail.com>
2021-12-12 21:24:49 +0100

    Implement extracting miliseconds from timestamps
    Also remove draft code, add tests.
    
    It is my first test ever! Written using TDD, this is hard but seems to
    be smart, my mentor tells me.

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import re
-with open('sample.pl') as f:
-    sample = f.read()
-RE_SUBTITLES = r'\d+\n(\d{2}):(\d{2}):(\d{2}),(\d{3}).*?\n(.*?)(?:\n\n|$)'
-re.findall(RE_SUBTITLES, sample, re.DOTALL)
+
+RE_SUBTITLES = re.compile(r'\d+\n(\d{2}):(\d{2}):(\d{2}),(\d{3}).*?\n(.*?)(?:\n\n|$)', re.DOTALL)
+
+
+def extract_from_srt(srt_contents):
+    result = []
+    for line in RE_SUBTITLES.findall(srt_contents):
+        h, m, s, ms, text = line
+        h, m, s, ms = int(h), int(m), int(s), int(ms)
+        result_ms = (h*60*60*1000)+(m*60*1000)+(s*1000)+ms
+        result.append((result_ms, text))
+    return result
+
--- test_app.py
+++ test_app.py
@@ -1,3 +1,35 @@
+from app import extract_from_srt
+
+
 def test_always_passes():
     """Just to check if pytest discovers the tests file."""
     pass
+
+
+def test_extracting_timestamps_and_subtitles_using_regex():
+    test_input = '''
+1
+00:00:06,000 --> 00:00:12,074
+line one
+
+2
+00:00:21,980 --> 00:00:24,596
+more
+than
+one
+line
+
+778
+11:53:23,305 --> 00:54:23,746
+the last line
+    '''.strip()
+    expected = [
+        (6000, 'line one'),
+        (21980, 'more\nthan\none\nline'),
+        ((11*60*60*1000)+(53*60*1000)+23305, 'the last line'),
+    ]
+    assert extract_from_srt(test_input) == expected
+
+
+def test_whole_file_just_count():
+    assert len(extract_from_srt(open('sample.pl').read())) == 767
